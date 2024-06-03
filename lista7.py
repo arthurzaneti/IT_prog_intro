7d2494b529e2406b41abb831d303757e6512e7a7
Arthur <afzaneti1@gmail.com>
2024-06-03 07:34:11 -0300

    File writing ok

--- lista7.py
+++ lista7.py
@@ -1,2 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-poly = lambda x: x**8 -3*x**4 + 2*x**3 - 2*x**2 - x + 2
+def write_data():
+    poly = lambda x: x**8 -3*x**4 + 2*x**3 - 2*x**2 - x + 2
+    x = np.linspace(-3/2, 3/2, 1000)
+    y = poly(x)
+
+    file = open("poly_graph_data.txt", "w")
+    for i in range(len(x)):
+        file.write(str((x[i], y[i])) + "\n")
+
+def read_data():
+    file = open("poly_graph_data.txt", "r")
+    data = file.readlines()
+    x = np.array()
+    y = np.array()
+    for i in range(len(data)):
+        point = data[i][0:len(data[i]) - 1].split(",")
+        print(point)
+read_data()
+print(np.array())
+
+
+
