fffd31603976d073e933a642d1040e339d8e9787
Arthur <afzaneti1@gmail.com>
2024-11-03 16:49:09 -0300

    lista6 done

--- lista6/lista6.py
+++ lista6/lista6.py
@@ -44,6 +44,8 @@ print(bfs(G, "A"))
 #______________________________________2____________________________________________
 def _bfs_search(G, starting_node, value):
     adj = G.adjacencias
+    if G.node_values[starting_node] == value:
+        return starting_node
     visited_nodes = [starting_node]
     f = Fila()
     for neighbour in adj[starting_node]:
@@ -64,7 +66,7 @@ def _bfs_search(G, starting_node, value):
 def bfs_search(G, starting_node, value): 
     #Preciso que essa função apenas coordene a busca e não realmente a faça para lidar com 
     #grafos disconexos. Essêncialmente ela so chama _bfs em cada componente conexo 
-    search = _bfs(G, starting_node)
+    search = _bfs_search(G, starting_node, value)
     if type(search) == str: #Achei o nó
         return(search)
 
@@ -72,7 +74,7 @@ def bfs_search(G, starting_node, value):
         for node in G.adjacencias.keys():
             if node not in search:
                 new_start = node
-        new_search = _bfs(G, new_start)
+        new_search = _bfs_search(G, new_start, value)
         if type(new_search) == str:
             return(new_search)
         else:
@@ -80,10 +82,11 @@ def bfs_search(G, starting_node, value):
     
     return(search) #Retorna todos os nós caso não ache o no procurado
 
+"""
 G = Graph()
 nodes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
 for node in nodes:
-    G.add_vertex(node, None)
+    G.add_vertex(node, node)
 edges = [
     ("A", "B"), ("A", "C"), ("B", "D"), ("C", "E"),
     ("F", "G"), ("G", "H"), ("H", "I"), ("I", "F"),
@@ -95,8 +98,162 @@ for x, y in edges:
 print(G)
 print(bfs_search(G, "A", "E"))  # Should find 'E'
 print(bfs_search(G, "F", "I"))  # Should find 'I'
-print(bfs_search(G, "J", "M"))  # Should not find 'M', return all nodes
-
+print(bfs_search(G, "J", "Q"))  # Should not find 'M', return all nodes
+"""
 #______________________________________3____________________________________________
+
+def count_islands(map):
+    visited_pos = []
+    island_count = 0
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 1 and (i,j) not in visited_pos:
+                visited_pos += _maximal_component(map, (i, j))
+                island_count +=1
+    return(island_count)
+
+def _maximal_component(map, pos):
+    component = []
+    f = Fila()
+    f.append((pos))
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1),  
+                  (-1, -1), (-1, 1), (1, -1), (1, 1)]
+    while not f.is_empty():
+        x, y = f.pop()
+        component.append((x, y))
+        for dx, dy in directions:
+            if (x + dx, y + dy) not in component: 
+                if 0 <= x + dx and x + dx < len(map) and 0 <= y + dy and y + dy < len(map[0]):
+                    if map[x + dx][y + dy] == 1:
+                        f.append((x + dx, y + dy))
+        
+    return(component)
+
+"""
+map_example = [
+    [0, 0, 1, 1, 0, 0, 0, 1],
+    [0, 1, 1, 0, 0, 1, 1, 1],
+    [0, 1, 0, 0, 1, 1, 0, 0],
+    [1, 1, 1, 0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0, 0, 0]
+]
+
+print(count_islands(map_example))
+"""
 #______________________________________4____________________________________________
+import numpy as np
+def centroids(map):
+    visited_pos = []
+    components = []
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 1 and (i,j) not in visited_pos:
+                component = _maximal_component(map, (i, j))
+                components.append(component)
+                visited_pos += component
+
+    smallest, biggest = float("inf"), 0
+    for i in range(len(components)):
+        if len(components[i]) < smallest:
+            smallest = len(component)
+            i_smallest = i
+        
+        if len(components[i]) > biggest:
+            biggest = len(component)
+            i_biggest = i
+    
+    centroid_smallest, centroid_biggest = [0,0], [0,0]
+    for pos in components[i_smallest]:
+        centroid_smallest[0] += pos[0]
+        centroid_smallest[1] += pos[1]
+    for pos in components[i_biggest]:
+        centroid_biggest[0] += pos[0]
+        centroid_biggest[1] += pos[1]
+
+
+    return((centroid_biggest[0] / len(components[i_biggest]),
+            (centroid_biggest[1] / len(components[i_biggest]))), 
+            (centroid_smallest[0] / len(components[i_smallest]),
+             centroid_smallest[1] / len(components[i_smallest])))
+"""
+map_example = [
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+# Call the centroids function
+centroid_biggest, centroid_smallest = centroids(map_example)
+print("Centroid of the biggest island:", centroid_biggest)
+print("Centroid of the smallest island:", centroid_smallest)
+"""
 #______________________________________5____________________________________________
+
+#Interpretei "4 direções" como não incluindo as diagonais
+
+def lake(map):
+    visited_pos = []
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if (i, j) not in visited_pos and map[i][j] == 1:
+                water_component, is_lake = _maximal_water_component(map, (i, j))
+                if is_lake == True:
+                    return(True)
+                visited_pos += water_component
+    
+    return(False)
+
+
+def _maximal_water_component(map, pos):
+    component = []
+    is_lake = True
+    f = Fila()
+    f.append((pos))
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    while not f.is_empty():
+        x, y = f.pop()
+        component.append((x, y))
+        for dx, dy in directions:
+            if (x + dx, y + dy) not in component: 
+                if 0 > x + dx or x + dx == len(map) or 0 > y + dy or y + dy == len(map[0]):
+                    is_lake = False
+                else:
+                    if map[x + dx][y + dy] == 1:
+                        f.append((x + dx, y + dy))
+    return(component, is_lake)
+
+"""
+no_lake = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+]
+
+with_lake = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0]
+]
+
+with_lake_2 = [# conectado diagonalmente com o oceano
+    [0, 0, 0, 0, 1],
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0]
+]
+
+print(lake(no_lake), lake(with_lake), lake(with_lake_2))
+"""
